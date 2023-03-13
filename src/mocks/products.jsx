7260bd295e5634068078b73589802383e7bd6fab
Martin Embeita <martinembeita@hotmmail.com>
2023-03-13 19:46:07 -0300

    se corrigen los productos

--- src/mocks/products.jsx
+++ src/mocks/products.jsx
@@ -4,10 +4,38 @@ import RoyalCanin from "../assets/imagenes/royalcanin.png";
 import Whiskas from "../assets/imagenes/whiskas.png";
 
 export default [
-  {id: 1, name: "PEDIGREE", price: 1500, description: "Alimento de alta nutrición de calidad para todos los perros", category: "perros", image: Pedigree,},
-  {id: 2, name: "PROPLAN", price: 2100, description: "Alimento diseñado para cumplir con las necesidades nutrimentales", category: "gatos", image: Proplan,},
-  {id: 3, name: "ROYAL CANIN", price: 1950, description: "Alimentos para perros balanceado con componentes naturales", category: "perros", image: RoyalCanin,},
-  {id: 4, name: "WHISKAS", price: 1600, description: "Alimento con minerales para mantener el tracto urinario sano", category: "gatos", image: Whiskas,},
+  {
+    id: 1, 
+    name: "PEDIGREE", 
+    price: 1500, 
+    description: "Alimento de alta nutrición de calidad para todos los perros", 
+    category: "perros", 
+    image: Pedigree,
+  },
+  {
+    id: 2, 
+    name: "PROPLAN", 
+    price: 2100, 
+    description: "Alimento diseñado para cumplir con las necesidades nutrimentales", 
+    category: "gatos", 
+    image: Proplan,
+  },
+  {
+    id: 3, 
+    name: "ROYAL CANIN", 
+    price: 1950, 
+    description: "Alimentos para perros balanceado con componentes naturales", 
+    category: "perros", 
+    image: RoyalCanin,
+  },
+  {
+    id: 4, 
+    name: "WHISKAS", 
+    price: 1600, 
+    description: "Alimento con minerales para mantener el tracto urinario sano", 
+    category: "gatos", 
+    image: Whiskas,
+  },
 ];
 
 
