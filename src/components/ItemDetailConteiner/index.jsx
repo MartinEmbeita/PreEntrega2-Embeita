74565b737ebc1ddc59c17e666997b6c474d5b36a
Martin Embeita <martinembeita@hotmmail.com>
2023-03-14 19:50:53 -0300

    se corrige el itemDetailConteiner

--- src/components/ItemDetailConteiner/index.jsx
+++ src/components/ItemDetailConteiner/index.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import ItemDetail from "../ItemDetail";
+import { useState, useEffect } from "react";
+import Products from "../../mocks/products";
 
-function ItemDetailContainer() {
-  return <div>Hello World!</div>;
+function ItemDetailContainer({ isItemRoute, itemId }) {
+  const [detalles, setDetalles] = useState({});
+
+  useEffect(() => {
+    const promiseDetail = new Promise((resolve, reject) => {
+      resolve(Products);
+    });
+
+    promiseDetail
+      .then((response) => {
+        if (isItemRoute) {
+          const productFind = response.find(
+            (item) => item.id === itemId
+          );
+          console.log();
+          setDetalles(productFind);
+        } else {
+          setDetalles(<h2>El producto no fue encontrado</h2>);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [itemId]);
+
+  return (
+    <div>
+      <div>Item Detail Container</div>
+      <ItemDetail items={detalles} />
+    </div>
+  );
 }
 
 export default ItemDetailContainer;
--- src/routes/item.jsx
+++ src/routes/item.jsx
@@ -1,11 +1,15 @@
 import NavBar from "../components/NavBar";
 import ItemDetailContainer from "../components/ItemDetailConteiner/index";
+import { useParams } from "react-router-dom";
 
 function ItemRoot() {
+  const params = useParams();
+  const isItemRoute = Boolean(params.id);
+
   return (
     <div className="App">
       <NavBar />
-      <ItemDetailContainer />
+      <ItemDetailContainer isItemRoute={isItemRoute} itemId={params.id} />
     </div>
   );
 }
