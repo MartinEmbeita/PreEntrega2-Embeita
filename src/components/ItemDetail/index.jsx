0d765917231104bed5425e2dc175fb37620c2f25
Martin Embeita <martinembeita@hotmmail.com>
2023-03-17 21:15:58 -0300

    se corrige estilo del detalle

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -8,15 +8,17 @@ const ItemDetail = () => {
   const product = Products.find((product) => product.id === parseInt(id));
 
   return (
-    <div className="item-detail-container">
-      <div className="item-detail-image-container">
-        <img className="item-detail-image" src={product.image} alt={product.name} />
-      </div>
-      <div className="item-detail-info-container">
-        <h2>{product.name} - ${product.price}</h2>
-        <p className="item-detail-description">{product.description}</p>
-        <ItemCount stock={product.stock} />
-        <button className="buy-button">Comprar</button>
+    <div className="item-detail-wrapper">
+      <div className="item-detail-container">
+        <div className="item-detail-image-container">
+          <img className="item-detail-image" src={product.image} alt={product.name} />
+        </div>
+        <div className="item-detail-info-container">
+          <h2>{product.name} - ${product.price}</h2>
+          <p className="item-detail-description">{product.detail}</p>
+          <ItemCount stock={product.stock} />
+          <button className="buy-button">Comprar</button>
+        </div>
       </div>
     </div>
   );
