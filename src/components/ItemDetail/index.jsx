ba67e2e39ad5dd2dc9a3bbe5ee9d8f4f8c56a115
Martin Embeita <martinembeita@hotmmail.com>
2023-03-18 21:27:55 -0300

    se agrega el buton

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,11 +1,17 @@
+import React from "react";
 import { ItemCount } from "../ItemCount/index";
 import Products from "../../mocks/products";
 import "./itemDetail.css";
 import { useParams } from "react-router-dom";
+import { Button } from "../Button/index";
 
 const ItemDetail = () => {
   const { id } = useParams();
   const product = Products.find((product) => product.id === parseInt(id));
+
+  const handleClick = () => {
+    alert("¡Has hecho clic en el botón Comprar!");
+  };
 
   return (
     <div className="item-detail-wrapper">
@@ -17,7 +23,7 @@ const ItemDetail = () => {
           <h2>{product.name} - ${product.price}</h2>
           <p className="item-detail-description">{product.detail}</p>
           <ItemCount stock={product.stock} />
-          <button className="buy-button">Comprar</button>
+          <Button onClick={handleClick} className="buy-button">Comprar</Button>
         </div>
       </div>
     </div>
@@ -25,4 +31,3 @@ const ItemDetail = () => {
 };
 
 export default ItemDetail;
-
